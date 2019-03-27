779ef295a0be8bf6fc104e6f7522d190c7953e56
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-27 08:45:16 -0600

    Updates to setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  # config.filter_sensitive_data('<API_KEY>') { ENV['API_KEY'] }
+  config.filter_sensitive_data('<NREL_API_KEY>') { ENV['NREL_API_KEY'] }
   # config.allow_http_connections_when_no_cassette = true
 end
 
