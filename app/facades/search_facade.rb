0375d3de24ec0a34b85404bc043bf5411bc3edda
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-03-31 15:40:08 -0600

    refactored

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -15,8 +15,7 @@ class SearchFacade
   end
 
   def response
-    conn = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_KEY']}&location=#{@zip}&fuel_type=LPG,ELEC&access=public&status=E&limit=15")
-    JSON.parse(conn.body, symbolize_names: true)
+    service.json
   end
 
   def service
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -2,4 +2,16 @@ class NRELService
   def initialize(zip)
     @zip = zip
   end
+
+  def json
+    response = conn.get("?api_key=#{ENV['NREL_KEY']}&location=#{@zip}&fuel_type=LPG,ELEC&access=public&status=E&limit=15")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json') do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
 end
